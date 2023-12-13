c22bf2c13a30f96e1a14b0d245de4ea041a0f8a2
leesuncom <70786826+leesuncom@users.noreply.github.com>
2023-12-13 11:09:20 +0800

    Add files via upload

---
+++ hosts.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+sed -i '/# GitHub520 Host Start/,/# Github520 Host End/d' /etc/hosts
+curl https://raw.hellogithub.com/hosts >> /etc/hosts
+/etc/init.d/dnsmasq restart
+/etc/init.d/AdGuardHome restart
+/etc/init.d/mosdns restart
